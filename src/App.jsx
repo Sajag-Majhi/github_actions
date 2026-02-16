da9a0b6c3efc2dc308e016a5831a99ca34e50c0a
Sajag-Majhi <saja649@hcoe.edu.np>
2026-02-16 20:53:14 +0545

    Krishna added again

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import Sajag from './Components/Sajag'
 import Ram from './Components/Ram'
 import Hari from './Components/Hari'
+import Krishna from './Components/Krishna'
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
       <Sajag/>
       <Ram/>
       <Hari/>
+      <Krishna/>
     </>
   )
 }
