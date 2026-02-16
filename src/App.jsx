2772e12728f210b23d79c20746126bf146e7e684
SajagMajhi <sajagmajhi666@gmail.com>
2026-02-16 20:54:02 +0545

    Shyam added

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import Sajag from './Components/Sajag'
 import Ram from './Components/Ram'
 import Hari from './Components/Hari'
+import Shyam from './Components/Shyam'
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
       <Sajag/>
       <Ram/>
       <Hari/>
+      <Shyam/>
     </>
   )
 }
