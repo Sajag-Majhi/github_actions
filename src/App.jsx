eab4917b4a73cf4b5508807a60cc4f2e675b2e16
SajagMajhi <sajagmajhi666@gmail.com>
2026-02-16 20:57:53 +0545

    removed

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,11 @@
 import './App.css'
 import Sajag from './Components/Sajag'
-import Ram from './Components/Ram'
-import Hari from './Components/Hari'
-import Shyam from './Components/Shyam'
-import Krishna from './Components/Krishna'
 
 function App() {
 
   return (
     <>
       <Sajag/>
-      <Ram/>
-      <Hari/>
-      <Shyam/>
-      <Krishna/>
     </>
   )
 }
